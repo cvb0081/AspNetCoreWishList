5ebeb8d81f3aa6641901805cf472faeff9a33148
Egi Xhaferaj <exhaferaj@ymail.com>
2021-08-31 18:41:45 +0200

    Add project files.

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
